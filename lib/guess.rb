8848c6f356bf1ee3e5f423d549578b53b45f04cb
Alicia Henzler <aliciahenzler@outlook.com>
2021-02-10 17:20:24 -0700

    create guess class/test

---
+++ lib/guess.rb
@@ -0,0 +1,17 @@
+
+class Guess
+  attr_reader :pegs
+
+  def initialize(pegs)
+    @pegs = pegs
+  end
+
+  def pegs_to_strings
+    string = ''
+    @pegs.each do |peg|
+      string += peg.color
+    end
+    return string.upcase
+  end
+
+end
