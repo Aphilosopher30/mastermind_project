c8a2bdc7220d907b760f2e7f0280270044d3166e
Alicia Henzler <aliciahenzler@outlook.com>
2021-02-10 21:59:20 -0700

    update sequence class/test

--- test/sequence_test.rb
+++ test/sequence_test.rb
@@ -24,6 +24,8 @@ class SequenceTest < Minitest::Test
     peg4 = Peg.new('b')
     sequence = Sequence.new([peg1, peg2, peg3, peg4])
 
+    assert_equal 0, sequence.just_correct_color_count
+    assert_equal 0, sequence.correct_place_color_count
     assert_equal [peg1, peg2, peg3, peg4], sequence.pegs
   end
 
@@ -69,6 +71,22 @@ class SequenceTest < Minitest::Test
 
     assert_equal false, sequence.compare_pegs?(guess1, 0)
     assert_equal true, sequence.compare_pegs?(guess2, 0)
+  end
+
+  def test_just_correct_colors_count
+    peg1 = Peg.new('r')
+    peg2 = Peg.new('g')
+    peg3 = Peg.new('y')
+    peg4 = Peg.new('b')
+    peg5 = Peg.new('g')
+    peg6 = Peg.new('r')
+    peg7 = Peg.new('b')
+    peg8 = Peg.new('y')
+    sequence = Sequence.new([peg1, peg2, peg3, peg4])
+    guess1 = Guess.new([peg5, peg6, peg7, peg8])
+    sequence.just_correct_colors_count(guess1)
+
+    assert_equal 4, sequence.just_correct_color_count
   end
 
 
