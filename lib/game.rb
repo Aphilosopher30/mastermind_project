6914bc78fcbe98c266d6b6ed937dd55478ed1584
Aphilosopher30 <Ashafer1@fordham.edu>
2021-02-12 18:11:44 -0700

    Add methods to game class

--- lib/game.rb
+++ lib/game.rb
@@ -4,10 +4,13 @@ require './lib/sequence'
 require './lib/turn'
 
 class Game
+  attr_reader :guess_count
+
   START_MESSAGE = "Welcome to MASTERMIND \n\nWould you like to (p)lay, read the (i)nstructions, or (q)uit?\n>"
   INSTRUCTIONS_MESSAGE = ""
 
   def initialize
+    @guess_count = 0
   end
 
   def start_message
@@ -19,9 +22,25 @@ class Game
   end
 
 
+  def incrament_guess_count
+    @guess_count += 1
+  end
 
 
   ############
+
+
+  #ask for input
+    #take string as input and check to see it is the  right length
+  # take string input and out put the message report
+
+  def evaluate_guess (turn)
+    turn.get_correct_placement_count
+    turn.get_correct_entities_count
+
+    feedback(turn.guess.pegs_to_strings, turn.correct_entities, turn.correct_placement, @guess_count)
+  end
+
 
 
 
--- test/game_test.rb
+++ test/game_test.rb
@@ -15,6 +15,7 @@ class GameTest < Minitest::Test
     assert_instance_of Game, game
   end
 
+
   def test_start_message
     game = Game.new
 
@@ -51,5 +52,46 @@ class GameTest < Minitest::Test
     assert_equal message, game.feedback(code, correct_element, correct_position, guess_count)
 
   end
+
+
+
+
+
+  def test_it_has_a_readable_guess_count
+    game = Game.new
+
+    assert_equal 0, game.guess_count
+  end
+
+  def test_evaluate_guess
+    game = Game.new
+
+    peg1 = Peg.new('r')
+    peg2 = Peg.new('g')
+    peg3 = Peg.new('y')
+    peg4 = Peg.new('b')
+    peg5 = Peg.new('r')
+    peg6 = Peg.new('g')
+    peg7 = Peg.new('y')
+    peg8 = Peg.new('b')
+
+    sequence = Sequence.new([peg1, peg2, peg3, peg4])
+    guess = Guess.new([peg5, peg6, peg7, peg8])
+
+    turn = Turn.new(sequence, guess)
+
+    feedback = "'RGYB' has 4 of the correct elements with 4 in the correct positions\nYou've taken 0 guess"
+
+
+    assert_equal feedback, game.evaluate_guess(turn)
+  end
+
+  def test_incrament_guess_count
+    game = Game.new
+    game.incrament_guess_count
+
+    assert_equal 1, game.guess_count
+  end
+
 
 end
