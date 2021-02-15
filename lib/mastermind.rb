c271a6a9144d60931f00ef85d778c47457920671
Alicia Henzler <aliciahenzler@outlook.com>
2021-02-15 11:59:51 -0700

    Update gameflow loop

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -8,13 +8,25 @@ require './lib/game'
 game = Game.new
 
 game.start_game
-
-game.start_time
-
-number_of_correct_guesses = 0
-while number_of_correct_guesses != game.secret_game_code.pegs.length
+restart = true
+while restart == true
+  restart = false
+  number_of_correct_guesses = 0
+  while number_of_correct_guesses != game.secret_game_code.pegs.length
+    input = game.get_input
+    number_of_correct_guesses = game.gameflow_input_response(input)
+  end
+  game.end_message
+  valid_input = false
+  while valid_input == false
   input = game.get_input
-  number_of_correct_guesses = game.gameflow_input_response(input)
+    if game.play(input)
+      game.begin_playing
+      valid_input = true
+      restart = true
+    else
+      game.quit(input)
+      puts "I'm sorry I do not understand this input, please try again."
+    end
+  end
 end
-
-game.end_message
