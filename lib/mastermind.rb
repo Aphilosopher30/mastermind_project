cf6cfb074c060ad078a368fd1be4d6b33b447189
Alicia Henzler <aliciahenzler@outlook.com>
2021-02-15 17:26:47 -0700

    Refactor of mastermind

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -7,16 +7,5 @@ require './lib/timer'
 
 
 game = Game.new
-
 game.start_game
-restart = true
-while restart == true
-  restart = false
-  number_of_correct_guesses = 0
-  while number_of_correct_guesses != game.secret_game_code.pegs.length
-    input = game.get_input
-    number_of_correct_guesses = game.gameflow_input_response(input)
-  end
-
-  restart = game.end_game
-end
+game.player_gameflow
