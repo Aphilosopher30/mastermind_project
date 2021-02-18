6c8c78b81d4f52d3e48f7d2a5dbd0967ee254ad3
Alicia Henzler <aliciahenzler@outlook.com>
2021-02-18 09:24:50 -0700

    add lost code

--- lib/game.rb
+++ lib/game.rb
@@ -72,6 +72,8 @@ class Game
   def player_gameflow
     restart = true
     while restart == true
+      # restart = false
+      # @guess_count = 0
       guess_the_code
       restart = end_game
     end
